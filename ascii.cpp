def91578632fd20bd3f9f936ef03f9a99888b77c
amdg <adcpetechsoft@hotmail.com>
2025-03-08 00:25:48 +0800

    mod001

--- ascii.cpp
+++ ascii.cpp
@@ -3,7 +3,32 @@
 int main(int argc, char *argv[])
 {
 
+    int x;
+    int y;
+    int j;
+    int k;
+
+    printf("\t");
+
+    x=2;
     
+    while (x<=7)
+    {
+        printf("%i\t", x);
+        x++;
+    };
+
+    printf("\n");
+
+    x=15;
+    while ( x>=0 )
+    {
+        printf("*");
+    };
+    
+    
+
+
 
 
     return 0;
